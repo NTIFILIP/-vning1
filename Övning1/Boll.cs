2b99e1609066f7f4140b3acc271ddc48cca78afe
Filip Hellgren <hellgren.filip@elev.ga.ntig.se>
2022-09-06 15:25:51 +0200

    Init

---
+++ Övning1/Boll.cs
@@ -0,0 +1,12 @@
+public class Boll {
+    uint color;
+    uint radius;
+    int x;
+    int y;
+    public Boll(uint color=0xFFFFFF, uint radius=0, int x=0, int y=0) {
+       this.color  = color;
+       this.radius = radius;
+       this.x = x;
+       this.y = y;
+    }
+}
---
+++ Övning1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+Boll b1 = new Boll(radius: 2);
+Boll b2 = new Boll(0, 5, 10, 10);
+Boll b3 = new Boll(20, 2, 1, 5);
+Boll b4 = new Boll(6, 129, -28 ,5);
+Boll b5 = new Boll(199, 29, 355, -392);
